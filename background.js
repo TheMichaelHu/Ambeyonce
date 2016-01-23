5f126887f0c5df6493632fba2a888eea6bcad61b
Michael <michaelanhu@gmail.com>
2016-01-23 00:43:18 -0500

    added indico api

--- background.js
+++ background.js
@@ -1,2 +1,49 @@
+// globals
 var audio = new Audio('http://datashat.net/music_for_programming_1-datassette.mp3');
-audio.play();
+audio.load()
+audio.addEventListener("load", function() { 
+	  audio.play(); 
+	});
+audio.addEventListener("ended", function() { 
+	  playNextSong();
+	});
+
+// functions
+function playNextSong() {
+	audio = new Audio(getNewSong(getMood()));
+	audio.load()
+}
+
+function getNewSong(mood) {
+	jQuery.get( "url", function(response) { 
+		var apiResponse = JSON.parse(response);
+		return apiResponse; //parse to get URL
+	});
+}
+
+function getMood() {
+	// single example
+	$.post(
+	  'https://apiv2.indico.io/fer',
+	  JSON.stringify({
+	    'data': "<IMAGE>" //Need the image
+	  })
+	).then(function(res) { 
+		console.log(res); 
+		return res;
+	});
+
+	// batch example
+	// $.post(
+	//   'https://apiv2.indico.io/fer/batch',
+	//   JSON.stringify({
+	//     'data': [
+	//       "<IMAGE>",
+	//       "<IMAGE>"
+	//     ]
+	//   })
+	// ).then(function(res) { console.log(res) });
+}
+
+// Main
+this.playNextSong();
